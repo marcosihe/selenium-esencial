611c9946d843fb5a1337e002e0a81a7730c13a59
marcosihe <marcosherrera.ib@gmail.com>
2021-11-14 15:54:07 -0300

    Add annotations such as priority and descriptions to the Tests

--- src/com/pruebaseleniumcontestng/Prueba.java
+++ src/com/pruebaseleniumcontestng/Prueba.java
@@ -4,17 +4,17 @@ import org.testng.annotations.Test;
 
 public class Prueba {
 
-    @Test
+    @Test ( priority = 1, description = "Test 1")
     void initialTest(){
         System.out.println("Test: OK");
     }
 
-    @Test
+    @Test ( priority = 2, description = "Test 2" )
     void initialTestUno(){
         System.out.println("Test: OK2");
     }
 
-    @Test
+    @Test ( priority = 3, description = "Test 3" )
     void initialTestDos(){
         System.out.println("Test: OK3");
     }
---
+++ src/com/pruebaseleniumcontestng/SecondClass.java
@@ -0,0 +1,4 @@
+package com.pruebaseleniumcontestng;
+
+public class SecondClass {
+}
