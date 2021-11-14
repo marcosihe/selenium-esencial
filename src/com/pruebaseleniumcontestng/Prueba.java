6c44317ca8b6161089a6ba15864c86d4b62333fe
marcosihe <marcosherrera.ib@gmail.com>
2021-11-14 15:41:51 -0300

    Configure testNG into the project

---
+++ src/com/pruebaseleniumcontestng/Prueba.java
@@ -0,0 +1,21 @@
+package com.pruebaseleniumcontestng;
+
+import org.testng.annotations.Test;
+
+public class Prueba {
+
+    @Test
+    void initialTest(){
+        System.out.println("Test: OK");
+    }
+
+    @Test
+    void initialTestUno(){
+        System.out.println("Test: OK2");
+    }
+
+    @Test
+    void initialTestDos(){
+        System.out.println("Test: OK3");
+    }
+}
